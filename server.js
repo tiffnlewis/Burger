35a2349d69f2fd2abf16f40d5410812e1913a040
Tiff L <tifftiffnlewis@gmail.com>
2019-03-11 15:07:43 -0500

    updated port information

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 var app = express();
 var exphbs = require('express-handlebars')
 var routes = require('./controllers/burgers_controllers.js')
